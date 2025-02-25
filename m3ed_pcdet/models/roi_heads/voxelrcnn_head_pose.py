e58ba93b95b3f87a689cd5ce18035058a7c0702b
AlanLiangC <Alan_Liang@aliyun.com>
2025-02-25 09:38:49 +0800

    fix bug

--- m3ed_pcdet/models/roi_heads/voxelrcnn_head_pose.py
+++ m3ed_pcdet/models/roi_heads/voxelrcnn_head_pose.py
@@ -73,7 +73,7 @@ class VoxelRCNNPoseHead(VoxelRCNNHead):
         z_params = self.featurizer(shared_features)
         z_dim = int(z_params.shape[-1]/2)
         z_mu, z_sigma = z_params[:,:z_dim], z_params[:,z_dim:]
-        z_sigma = F.softplus(z_sigma) 
+        z_sigma = F.softplus(z_sigma) + 1e-6
         z_dist = dist.Independent(dist.normal.Normal(z_mu,z_sigma),1)
         z = z_dist.rsample()
         z_cls = self.classifier(z)
@@ -96,10 +96,6 @@ class VoxelRCNNPoseHead(VoxelRCNNHead):
 
         return batch_dict
 
-    def kl_divergence(self, posterior_latent_space, prior_latent_space):
-        kl_div = kl.kl_divergence(posterior_latent_space, prior_latent_space)
-        return kl_div
-
     def get_z_box_cls_layer_loss(self, forward_ret_dict):
         loss_cfgs = self.model_cfg.LOSS_CONFIG
         rcnn_cls = forward_ret_dict['z_cls']
@@ -107,11 +103,11 @@ class VoxelRCNNPoseHead(VoxelRCNNHead):
         if loss_cfgs.CLS_LOSS == 'BinaryCrossEntropy':
             rcnn_cls_flat = rcnn_cls.view(-1)
             batch_loss_cls = F.binary_cross_entropy(torch.sigmoid(rcnn_cls_flat), rcnn_cls_labels.float(), reduction='none')
-            cls_valid_mask = (rcnn_cls_labels > 0).float()
+            cls_valid_mask = (rcnn_cls_labels >= 0).float()
             rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum() / torch.clamp(cls_valid_mask.sum(), min=1.0)
         elif loss_cfgs.CLS_LOSS == 'CrossEntropy':
             batch_loss_cls = F.cross_entropy(rcnn_cls, rcnn_cls_labels, reduction='none', ignore_index=-1)
-            cls_valid_mask = (rcnn_cls_labels > 0).float()
+            cls_valid_mask = (rcnn_cls_labels >= 0).float()
             rcnn_loss_cls = (batch_loss_cls * cls_valid_mask).sum() / torch.clamp(cls_valid_mask.sum(), min=1.0)
         else:
             raise NotImplementedError
@@ -124,7 +120,7 @@ class VoxelRCNNPoseHead(VoxelRCNNHead):
         rcnn_loss, tb_dict = super().get_loss(tb_dict)
 
         z_loss_cls, cls_tb_dict = self.get_z_box_cls_layer_loss(self.forward_ret_dict)
-        rcnn_loss += z_loss_cls
+        rcnn_loss += z_loss_cls * 0.1
         tb_dict.update(cls_tb_dict)
         tb_dict['rcnn_loss'] = rcnn_loss.item()
         return rcnn_loss, tb_dict
